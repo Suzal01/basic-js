3890a0c46e64159cd1022382c34dd74296fac899
Suzal01 <suzal470@gmail.com>
2024-04-23 23:55:56 +0545

    C:/Program Files/Git/Snapshot

---
+++ project.js
@@ -0,0 +1,133 @@
+/*let age=16;
+ if(age>=18)
+ {
+    console.log("you can vote");
+ }
+ if(age<=18)
+ {
+    console.log("you cannot vote");
+ }*/
+ /*let a=10;
+ if(a%2==0)
+ {
+    console.log("even");
+ }
+ else
+ {
+    console.log("odd");
+
+ }*/
+
+/*let mode="dark"; //variable
+let color; //to set
+if (mode=="dark")
+{
+    console.log("black");
+}
+if (mode=="light")
+{
+    console.log("white");
+}
+console.log(color);*/
+
+/*let mode="light";
+ let color;
+ if(mode=="dark")
+{
+
+    color="black";
+}
+else{
+    color="white";
+}
+console.log(color);*/
+/*let age=30;
+if(age<=18)
+{
+    console.log("junior");
+}
+else if(age<=60)
+{
+    console.log("middle");
+}
+else
+{
+    console.log("adult");
+}*/
+/*let mode="dark";
+let color;
+if(mode=="dark")
+{
+    color="black";
+}
+else if(mode=="blue")
+{
+    color="blue";
+}
+else if(mode=="pink")
+{
+color="pink";
+}
+else
+{
+    color="white";
+}
+
+console.log(color);*/
+/*let age=16;
+let result= age>=18 ? "adult":"not adult";
+console.log(result);
+//OR
+let age=25;
+age>18 ? console.log("adult"):console.log("not adult");*/
+
+/*let number= prompt("Enter a number");
+if(number%5==0)
+{
+    console.log("divisible by 5");
+}
+else{
+    console.log("not divisible by 5");
+}*/
+
+//alert("hello!");
+ /*let name=prompt("hi!");
+ console.log(name);*/
+
+/*let marks=72;
+let grade;
+if(marks>=90 && marks<=100)
+{
+    grade="A";
+}
+else if(marks>=70 && marks<=89)
+{
+    grade="B";
+
+}
+else if(marks>=60 && marks<=69)
+{
+    grade="C";
+
+}
+else if(marks>=50 && marks<=59)
+{
+    grade="D";
+
+}
+else if(marks>=0 && marks<=49)
+{
+    grade="F";
+
+}
+console.log(grade)*/
+/*for(let i=1; i<=5; i++)
+{
+    console.log("Nepal");
+}*/
+
+/*for(let i=1;i<=5;i++)
+{
+    cosole.log(i);
+}*/
+
